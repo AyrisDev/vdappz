7a7a1476908af32c2a0de8abab97e27a56118cf1
mstfyldz <mstfyldz@mstfyldz-MacBook-Pro.local>
2023-07-04 01:55:39 +0300

    vce

--- src/hooks/useVince.ts
+++ src/hooks/useVince.ts
@@ -5,13 +5,13 @@ const VCE = {
   coinMinimalDenom: 'avce',
   coinDecimals: 18,
   coinGeckoId: 'vince',
-  coinImageUrl: 'https://dhj8dql1kzq2v.cloudfront.net/white/osmo.png',
+  coinImageUrl: 'https://github.com/AyrisDev/vinceChainAssets/blob/main/icon.png',
 };
 
 export const vinceTestnet = {
   rpc: process.env.NEXT_PUBLIC_RPC_URL,
   rest: process.env.NEXT_PUBLIC_REST_URL,
-  chainId: 'vince_1903-1',
+  chainId: 'vince_1903',
   chainName: 'VinceChain ',
   stakeCurrency: VCE,
   bip44: {
